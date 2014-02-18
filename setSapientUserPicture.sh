5258dcf281f7ebafc5fd308dfbe1bfb582a69411
Han Su Kim <hkim823@gmail.com>
2014-02-18 14:43:44 -0500

    deleted setSapientUserPicture.sh

--- setSapientUserPicture.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#set A_Sapient_Logo User Pictures
-
-dscl . delete /Users/lsadmin Picture
-dscl . delete /Users/lsadmin JPEGPhoto
-dscl . create /Users/lsadmin Picture "/Library/User Pictures/Sapient/A_Sapient_Logo.tif"
-
-exit 0
